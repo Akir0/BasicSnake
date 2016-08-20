b911e55aa4f884e073c1a196ec233032bad39c48
Akir0 <karimsaadani94@gmail.com>
2016-08-20 14:01:47 +0200

    Add system of score.
    System of score added in game with a text of victory in the end.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed; //Variable contenant la vitesse de la balle changeable dans l'éditeur Unity grâce à son accès "public".
+	public Text scoreText; //Variable contenant le texte du score.
+	public Text winText; //Variable contenant le texte de victoire.
+
     private Rigidbody rB; //Variable pour accéder au "Rigidbody" de la balle. Le "Rigidbody" contient les aspects physiques d'un objet. Cela rend l'objet solide et soumis à la gravité.
+	private int score; //Variable contenant le score du joueur.
 
     void Start () //Démarrage du jeu (initialisation)
     {
         rB = GetComponent<Rigidbody>(); //Recherche du "Rigidbody" de la balle et stockage dans la variable "rB". Execution au démarrage du jeu pour éviter de surcharger l'ordinateur.
+		score = 0; //Au départ, le joueur n'a pas de point.
+		SetScoreText(); //Affichage du score au début de partie.
+		winText.text = "";
     }
      
     void FixedUpdate()
@@ -25,6 +33,18 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Miam")) //On teste si l'objet de la collision est taggé "Miam".
         {
             other.gameObject.SetActive(false); //Si c'est le cas, on désactive l'objet. Il disparaît.
+			score = score + 1; //À chaque collision, le score s'incrémente et augmente.
+			SetScoreText(); //Affichage du score au fur et mesure qu'il augmente.
         }
     }
+
+	void SetScoreText () //Fonction d'affichage du score.
+	{
+		scoreText.text = "Score : " + score.ToString ();
+
+		if (score == 6)
+		{
+			winText.text = "You win !!! :D";
+		}
+	}
 }
