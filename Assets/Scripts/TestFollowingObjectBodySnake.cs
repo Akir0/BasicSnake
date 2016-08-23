9dc45de16e71e7c5d4a4331997fc373a9d9b3a2d
Akir0 <karimsaadani94@gmail.com>
2016-08-23 17:15:00 +0200

    Test fail

---
+++ Assets/Scripts/TestFollowingObjectBodySnake.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestFollowingObjectBodySnake : MonoBehaviour
+{
+    Transform player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+    }
+}
