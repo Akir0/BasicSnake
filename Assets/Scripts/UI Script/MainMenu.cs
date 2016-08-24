5c4ebf2609d83606c90130e09954522a6a05d849
Akir0 <karimsaadani94@gmail.com>
2016-08-24 15:58:08 +0200

    Added Main menu for the game.

---
+++ Assets/Scripts/UI Script/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainSceneSnakeGame");
+    }
+
+    public void HighScore()
+    {
+
+    }
+
+    public void Credits()
+    {
+
+    }
+}
